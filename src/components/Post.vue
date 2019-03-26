c92081c59b5550fc952832c67174d5006b13ca85
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-26 12:42:37 -0600

    created Component to render posts

---
+++ src/components/Post.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>{{postObj.author}}</p>
+        <p>{{postObj.post}}</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['postObj']
+}
+</script>
