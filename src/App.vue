fd0171e0b1d7e564f1b379426191c15a81400e77
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-26 12:05:44 -0600

    created a form for adding a new post, added data and method for adding post

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
       <button @click="toggle">Click for Awesome</button>
       <section v-if='showForm'>
         <h3>Add new blog post:</h3>
+        <p>
+          Author: <input type="text" v-model = "author">
+        </p>
+        <p>
+          <textarea name="" id="" cols="30" rows="10" v-model="post"></textarea>
+          <br>
+          <button @click="addPost">Add Post</button>
+        </p>
       </section>
       <section v-else>
         <h3>All posts:</h3>
@@ -17,12 +25,22 @@ export default {
   name: 'app',
   data() {  //don't use arrow functions in vue, but you can use ES6 method syntax.
     return {
-      showForm:true
+      showForm:true,
+      author:"",
+      posts:"",
+      allPosts:[]
     }
   },
   methods:{
     toggle: function() {
       this.showForm = !this.showForm
+    },
+    addPost: function() {
+      const newPost = {author: this.author, post: this.post};
+      this.allPosts.push(newPost)
+      this.author ='';
+      this.author = '';
+      this.toggle();
     }
   }
 }
