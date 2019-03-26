9a7d0a604e99a92b8a65e03ff276ac5d277f87e8
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-26 11:51:59 -0600

    added a toggle button that toggles between two divs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
       <h1> Awesome Blogs </h1>
+      <button @click="toggle">Click for Awesome</button>
+      <section v-if='showForm'>
+        <h3>Add new blog post:</h3>
+      </section>
+      <section v-else>
+        <h3>All posts:</h3>
+      </section>
+      
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data: function() {
+  data() {  //don't use arrow functions in vue, but you can use ES6 method syntax.
     return {
-
+      showForm:true
+    }
+  },
+  methods:{
+    toggle: function() {
+      this.showForm = !this.showForm
     }
   }
 }
